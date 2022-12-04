6208e328c9c2c729481bace8ea4a39c944181319
Maurício Conde Ramon <92527061+mcondero@users.noreply.github.com>
2022-12-04 10:10:54 -0300

    atualização do projeto

--- src/controller/routes/createUser.go
+++ src/controller/routes/createUser.go
@@ -1,5 +1,18 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gitlab.com/mauriciocondero/meu-primeiro-crud-em-go/src/configuration/rest_err"
+	"k8s.io/apiserver/pkg/admission/plugin/webhook/request"
+)
 
-func CreateUser(c *gin.Context) {}
+func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil{
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err)
+		)
+	}
+
+}
